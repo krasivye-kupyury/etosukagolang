a8154fbe7bd5155725c83e5d2b71e9451700739c
krasivye-kupyury <chasikvradost@yandex.ru>
2026-02-08 21:39:20 +0300

    вроде работает, проверку на входные данные не делал

--- pervyialgoritm.go
+++ pervyialgoritm.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var k, c int
+	var k, c, t int
 	fmt.Println("введите кол-во произнесенных чисел")
 	fmt.Scan(&k)
+	t = 0
 	a := make([]int, k)
 	fmt.Println("введите последовательность поддтягиваний")
 	for index := range a {
@@ -20,20 +21,25 @@ func main() {
 	fmt.Println("количесвто подходов")
 	fmt.Println(c)
 	b := make([]int, c)
-	fmt.Println("массив:")
-
-	for i := 0; i < len(a); i++ {
-
-		for j := range b {
+	fmt.Println("количесвто подтягиваний в каждом подходе")
+	for j := range b {
+		for i := t; i < len(a)-1; i++ {
 			if a[i+1] == 1 {
+
 				b[j] = a[i]
+				t = t + 1
+				break
+			}
+			if a[i+1] != 1 {
+				t = t + 1
+
 			}
 
 		}
-
 	}
-	for index := range b {
-		fmt.Println(b[index])
+	b[c-1] = a[k-1]
+	for j := range b {
+		fmt.Println(b[j])
 	}
 
 }
