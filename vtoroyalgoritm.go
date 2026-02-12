416a34bd0f6f74dab252c0d8634b1fa51c1a9cc4
krasivye-kupyury <chasikvradost@yandex.ru>
2026-02-12 17:21:26 +0300

    для моего уровня очень хорошо

--- vtoroyalgoritm.go
+++ vtoroyalgoritm.go
@@ -2,111 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
+var letN, letW, letS, letE = "n", "w", "s", "e"
+var min = 0.001
+var max = 99.999
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	min := 0.001
-	max := 99.999
 	var s string
-	var n, w, ss, e, b bool
-	b = false
-	n = false
-	w = false
-	ss = false
-	e = false
-	var valueN, valueW, valueS, valueE, x, y float64
-	var countN, countW, countS, countE int64
+	var valueN, valueW, valueS, valueE, x, y = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+	var countN, countW, countS, countE = 0, 0, 0, 0
 	var sumN, sumW, sumS, sumE float64
-	x = 0.0
-	y = 0.0
-	countN = 0
-	countW = 0
-	countS = 0
-	countE = 0
-	valueE = 0
-	valueN = 0
-	valueS = 0
-	valueW = 0
+	var n, w, ss, e, b = false, false, false, false, false
 	for {
-		if b == false {
-			fmt.Println("введите строку")
-			fmt.Scan(&s)
-			for _, runevalue := range s {
-				if (string(runevalue) != "n") && (string(runevalue) != "w") && (string(runevalue) != "s") && (string(runevalue) != "e") {
-					fmt.Println("ошибка, можно вводить только w s e n")
-					b = false
-					break
-				} else {
-					b = true
-				}
-			}
-		} else {
+		fmt.Println("введите строку")
+		fmt.Scan(&s)
+		s, b = proverka(s, b)
+		if b == true {
 			break
 		}
 	}
-	for _, runevalue := range s {
-		if string(runevalue) == "n" {
-			n = true
-			countN = countN + 1
-		}
-		if string(runevalue) == "w" {
-			w = true
-			countW = countW + 1
-		}
-		if string(runevalue) == "s" {
-			ss = true
-			countS = countS + 1
-		}
-		if string(runevalue) == "e" {
-			e = true
-			countE = countE + 1
-		}
-	}
+	countN, n = schetbukv(letN, s)
+	countE, e = schetbukv(letE, s)
+	countS, ss = schetbukv(letS, s)
+	countW, w = schetbukv(letW, s)
 	if e == true {
 		if w == true {
-			valueE = min + rand.Float64()*(max-min)
-			sumE = valueE * float64(countE)
-			sumW = sumE
-			valueW = sumW / float64(countW)
+			valueE, valueW, sumE, sumW = schetkoord1(countE, countW)
 			w = false
 		} else {
-			valueE = min + rand.Float64()*(max-min)
+			valueE, sumE = schetkoord2(countE)
 		}
 	}
 	if n == true {
 		if ss == true {
-			valueN = min + rand.Float64()*(max-min)
-			sumN = valueN * float64(countN)
-			sumS = sumN
-			valueS = sumS / float64(countS)
+			valueN, valueS, sumN, sumS = schetkoord1(countN, countS)
 			ss = false
 		} else {
-			valueN = min + rand.Float64()*(max-min)
+			valueN, sumN = schetkoord2(countN)
 		}
 	}
 	if w == true {
 		if e == true {
-			valueW = min + rand.Float64()*(max-min)
-			sumW = valueW * float64(countW)
-			sumE = sumW
-			valueE = sumE / float64(countE)
+			valueW, valueE, sumW, sumW = schetkoord1(countW, countE)
 			e = false
 		} else {
-			valueW = min + rand.Float64()*(max-min)
+			valueW, sumW = schetkoord2(countW)
 		}
 	}
 	if ss == true {
 		if n == true {
-			valueS = min + rand.Float64()*(max-min)
-			sumS = valueS * float64(countS)
-			sumN = sumS
-			valueN = sumN / float64(countN)
+			valueS, valueN, sumS, sumN = schetkoord1(countS, countN)
 			n = false
 		} else {
-			valueS = min + rand.Float64()*(max-min)
+			valueS, sumS = schetkoord2(countS)
 		}
 	}
 	fmt.Println("N =", valueN, "kolich N =", countN, "summa N=", sumN)
@@ -114,29 +67,76 @@ func main() {
 	fmt.Println("S =", valueS, "kolich S =", countS, "summa S=", sumS)
 	fmt.Println("E =", valueE, "kolich E =", countE, "summa E=", sumE)
 	for _, runevalue := range s {
-		if string(runevalue) == "n" {
+		if string(runevalue) == letN {
 			y = y + valueN
-			fmt.Println(x, " ; ", y)
+			fmt.Printf("%.6f %.6f\n", x, y)
 		}
-		if string(runevalue) == "w" {
+		if string(runevalue) == letW {
 			x = x - valueW
-			fmt.Println(x, " ; ", y)
+			fmt.Printf("%.6f %.6f\n", x, y)
 		}
-		if string(runevalue) == "s" {
+		if string(runevalue) == letS {
 			y = y - valueS
-			fmt.Println(x, " ; ", y)
+			fmt.Printf("%.6f %.6f\n", x, y)
 		}
-		if string(runevalue) == "e" {
+		if string(runevalue) == letE {
 			x = x + valueE
-			fmt.Println(x, " ; ", y)
+			fmt.Printf("%.6f %.6f\n", x, y)
 		}
+	}
+	if x > 0 {
+		x = math.Floor(x)
+	}
+	if y > 0 {
+		y = math.Floor(y)
+	}
+	if x < 0 {
+		x = math.Ceil(x)
+	}
+	if y < 0 {
+		y = math.Ceil(y)
 	}
 	if x == 0 && y == 0 {
 		fmt.Println("yes")
 	} else {
 		fmt.Println("no")
-		fmt.Println("если у одной из координат на конце ...е-..")
-		fmt.Println("то это обозначает 0 и на самом деле вывод должен быть yes")
-		fmt.Println("это происходит из-за погрешности при работе с float, как это пофиксить я пока хз")
 	}
+}
+func proverka(stroka string, bb bool) (string, bool) {
+	for _, runevalue := range stroka {
+		if (string(runevalue) != letN) && (string(runevalue) != letW) && (string(runevalue) != letS) && (string(runevalue) != letE) {
+			fmt.Println("ошибка, можно вводить только w s e n")
+			fmt.Println("попробуйте еще раз")
+			bb = false
+			break
+		} else {
+			bb = true
+		}
+	}
+	return stroka, bb
+}
+func schetbukv(bukva string, ss string) (int, bool) {
+	var count = 0
+	var bb bool
+	for _, runevalue := range ss {
+		if string(runevalue) == bukva {
+			bb = true
+			count = count + 1
+		}
+	}
+	return count, bb
+}
+func schetkoord1(countA int, countB int) (float64, float64, float64, float64) {
+	var valueA, valueB, sumA, sumB float64
+	valueA = math.Round((min+rand.Float64()*(max-min))*1_000_000) / 1_000_000
+	sumA = valueA * float64(countA)
+	sumB = sumA
+	valueB = sumB / float64(countB)
+	return valueA, valueB, sumA, sumB
+}
+func schetkoord2(countA int) (float64, float64) {
+	var valueA, sumA float64
+	valueA = math.Round((min+rand.Float64()*(max-min))*1_000_000) / 1_000_000
+	sumA = valueA * float64(countA)
+	return valueA, sumA
 }
